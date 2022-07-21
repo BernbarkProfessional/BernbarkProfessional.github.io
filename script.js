6193719856c72386b1a104bd3c5dcd31e21c6898
Bernbark <korystennett@yahoo.com>
2022-07-21 05:28:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,32 @@ window.onload=function(){
     .then(data=>{
         sidebar.innerHTML = data
     })
-    let dropdownBtn = document.querySelector('.menu-btn');
-    let menuContent = document.querySelector('.menu-content');
-    if(dropdownBtn != null){
-        dropdownBtn.addEventListener('click',()=>{
-            if(menuContent.style.display===""){
-                menuContent.style.display="block";
-            } else {
-                menuContent.style.display="";
+    //let dropdownBtn = document.querySelector('.menu-btn');
+    const dropdownBtn
+    const dropdownBtnFunction = async () => {
+        dropdownBtn = await setDropDownButton();
+        let menuContent = document.querySelector('.menu-content');
+            if(dropdownBtn != null){
+                dropdownBtn.addEventListener('click',()=>{
+                    if(menuContent.style.display===""){
+                        menuContent.style.display="block";
+                    } else {
+                        menuContent.style.display="";
+                    }
+                })
+                dropdownBtn.addEventListener('mouseout',()=>{
+                    
+                    menuContent.style.display="";
+                    
+                })
             }
-        })
-        dropdownBtn.addEventListener('mouseout',()=>{
-            
-            menuContent.style.display="";
-            
-        })
+        }
     }
+    dropdownBtnFunction();
+    
+
+function setDropDownButton(){
+    let dropdownBtn = document.querySelector('.menu-btn');
 }
 
 
