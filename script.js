70eab543ccbb932f98ec3d357abff09bd92d1f84
Bernbark <korystennett@yahoo.com>
2022-07-22 19:51:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 
-
-const sidebar = document.querySelector('sidebar')
+/*      Loading Section for Common HTML Elements        */
+const sidebar = document.querySelector('#sidebar')
 fetch('/sidebar.html')
 .then(res=>res.text())
 .then(data=>{
     sidebar.innerHTML = data
 })
-const bottombar = document.querySelector('bottombar')
+const bottombar = document.querySelector('#bottombar')
 fetch('/bottombar.html')
 .then(res=>res.text())
 .then(data=>{
     bottombar.innerHTML = data
 })
+
+/*      End Loading of Common HTML Elements     */
+
+
 window.onload = function(e){
     let dropdownBtn = document.querySelector('.menu-btn');
     let menuContent = document.querySelector('.menu-content');
