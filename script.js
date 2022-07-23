ad79d68080888c0f81a34c0cee957b4927af94b1
Bernbark <korystennett@yahoo.com>
2022-07-22 17:27:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,20 +9,20 @@ fetch('/sidebar.html')
 window.onload = function(e){
     let dropdownBtn = document.querySelector('.menu-btn');
     let menuContent = document.querySelector('.menu-content');
-    if(dropdownBtn != null){
-        dropdownBtn.addEventListener('click',()=>{
-            if(menuContent.style.display===""){
-                menuContent.style.display="block";
-            } else {
-                menuContent.style.display="";
-            }
-        })
-        dropdownBtn.addEventListener('mouseout',()=>{
-            
+    
+    dropdownBtn.addEventListener('click',()=>{
+        if(menuContent.style.display===""){
+            menuContent.style.display="block";
+        } else {
             menuContent.style.display="";
-            
-        })
-    }
+        }
+    })
+    dropdownBtn.addEventListener('mouseout',()=>{
+        
+        menuContent.style.display="";
+        
+    })
+    
 }
 
 
