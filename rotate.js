ca3cfbcbd7cbdf964844dd12c19d0e71ae585115
Bernbark <korystennett@yahoo.com>
2022-07-30 23:31:07 -0700

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -1,9 +1,9 @@
 setTimeout(function(){
-    var rotateOn = document.querySelector("rotate-button");
+    var rotateOn = document.querySelector('rotate-button');
     function updateDebugState() {
-        let archiveWrapper = document.querySelector("archive-body");
+        let archiveWrapper = document.querySelector('archive-body');
         archiveWrapper.classList.toggle('rotate-on', rotateOn.checked);
     };
-    rotateOn.addEventListener("click", updateDebugState);
+    rotateOn.addEventListener('click', updateDebugState);
     updateDebugState();
 },250)
