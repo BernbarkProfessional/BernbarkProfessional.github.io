05b34d16f2ea87dd0caf2bc11772ca136a4c679f
Bernbark <korystennett@yahoo.com>
2022-08-17 16:37:47 -0700

    Adding new thing to buy and making button interactions better

--- idle_game.js
+++ idle_game.js
@@ -4,6 +4,10 @@ var oldCouches = 0;
 var oldCouchesCost = 20;
 var couchIncome = 1;
 
+var crackedTV = 0;
+var crackedTVCost = 100;
+var crackedTVIncome = 5;
+
 // Buying Section
 
 function buyOldCouch() {
@@ -11,6 +15,17 @@ function buyOldCouch() {
         gold = gold - oldCouchesCost;
         oldCouches = oldCouches + 1;
         oldCouchesCost = Math.round(oldCouchesCost * 1.14);
+        updateUI();
+        saveGame();
+        
+    }
+}
+
+function buyCrackedTV() {
+    if(gold >= crackedTVCost){
+        gold = gold - crackedTVCost;
+        crackedTV = crackedTV + 1;
+        crackedTVCost = Math.round(crackedTVCost * 1.145);
         updateUI();
         saveGame();
         
@@ -27,10 +42,13 @@ function updateUI(){
     document.getElementById("gold").innerHTML = "Gold: " +gold;
     document.getElementById("oldCouches").innerHTML = "Couches: "+oldCouches;
     document.getElementById("oldCouchesCost").innerHTML = "Old Couch: $"+oldCouchesCost;
+    document.getElementById("crackedTVs").innerHTML = "TVs: "+crackedTV;
+    document.getElementById("crackedTVCost").innerHTML = "Cracked TV: $"+crackedTVCost;
 }
 
 setInterval(function(){
     gold = gold + (couchIncome * oldCouches);
+    gold = gold + (crackedTVIncome * crackedTV);
     updateUI();
 }, 1000);
 
