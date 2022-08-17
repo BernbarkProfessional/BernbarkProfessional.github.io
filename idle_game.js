e07f257dfde6e84ecee91980e5004409e1e65841
Bernbark <korystennett@yahoo.com>
2022-08-17 15:36:59 -0700

    Trying to get idle game saving working

---
+++ idle_game.js
@@ -0,0 +1,59 @@
+var gold = 0;
+
+var oldCouches = 0;
+var oldCouchesCost = 20;
+var couchIncome = 1;
+
+// Buying Section
+
+function buyOldCouch() {
+    if(gold >= oldCouchesCost){
+        gold = gold - oldCouchesCost;
+        oldCouches = oldCouches + 1;
+        oldCouchesCost = Math.round(oldCouchesCost * 1.14);
+        updateUI();
+
+        
+    }
+}
+
+function addToScore(amount){
+    gold = gold + amount;
+    updateUI();
+}
+
+function updateUI(){
+    document.getElementById("gold").innerHTML = "Gold: " +gold;
+    document.getElementById("oldCouches").innerHTML = "Couches: "+oldCouches;
+    document.getElementById("oldCouchesCost").innerHTML = "Old Couch: $"+oldCouchesCost;
+}
+
+setInterval(function(){
+    gold = gold + (couchIncome * oldCouches);
+    updateUI();
+}, 1000);
+
+function saveGame(){
+    var gameSave = {
+        gold: gold,
+        oldCouches: oldCouches,
+        oldCouchesCost: oldCouchesCost
+    };
+    localStorage.setItem("gameSave",JSON.stringify(gameSave));
+}
+
+function loadGame(){
+    var savedGame = JSON.parse(localStorage.getItem("gameSave"));
+    if(typeof savedGame.gold !== "undefined") score = savedGame.gold;
+    if(typeof savedGame.oldCouches !== "undefined") score = savedGame.oldCouches;
+    if(typeof savedGame.oldCouchesCost !== "undefined") score = savedGame.oldCouchesCost;
+}
+
+setInterval(function(){
+    saveGame();
+}, 30000)
+
+window.onload = function() {
+    loadGame();
+    updateUI();
+};
